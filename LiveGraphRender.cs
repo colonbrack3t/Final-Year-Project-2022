456762259e8e360845aeaca6dba332c3f43da7c2
Ludo <ludoattuoni@gmail.com>
2022-02-27 16:11:34 +0000

    Real time Board readings rep

---
+++ LiveGraphRender.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveGraphRender : MonoBehaviour
+{
+    [SerializeField] BalanceBoardSensor bbs;
+    [SerializeField] bool start;
+    [SerializeField] Sensor s;
+    [SerializeField] LineRenderer lr;
+    float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            lr.positionCount += 1;
+            time += Time.deltaTime;
+            float z =0;
+            switch (s)
+            {
+                case Sensor.TopLeft:
+                    z = (float)bbs.rwTopLeft;
+                    break;
+                case Sensor.TopRight:
+                    z = (float)bbs.rwTopRight;
+                    break;
+                case Sensor.BottomLeft:
+                    z = (float)bbs.rwBottomLeft;
+                    break;
+                case Sensor.BottomRight:
+                    z = (float)bbs.rwBottomRight;
+                    break;
+            }
+            lr.SetPosition(lr.positionCount - 1, new Vector3(time, -10, z));
+        }
+    }
+    enum Sensor { TopLeft, TopRight, BottomLeft, BottomRight }
+}
