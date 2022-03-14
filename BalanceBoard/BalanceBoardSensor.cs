8f72dbeea0240914a0ba5f7250269bfd2131e035
Ludo Attuoni <ludoattuoni@gmail.com>
2022-03-14 13:20:41 +0000

    removing recording from bbs, can now use data capture script

--- BalanceBoard/BalanceBoardSensor.cs
+++ BalanceBoard/BalanceBoardSensor.cs
@@ -32,22 +32,7 @@ public class BalanceBoardSensor : MonoBehaviour
     public double weight = 0;
     private int num_weight_readings = 0;
     //Text visual component
-    [SerializeField] private Text UItext;
-    //Headset "Camera" transform - matches where headset is in real world
-    [SerializeField] private Transform headsetcamera;
-
-    //record headset/record board sensors flags
-    [SerializeField] public bool record_headset = false, record_board_collision = false;
-
-    // Lists to store board/headset/mouse clicks
-
-    //saves
-    private List<double[]>[] board_recording = new List<double[]>[]{new List<double[]>(),new List<double[]>(),new List<double[]>(),new List<double[]>()};
-    private List<double[]> Headset_record = new List<double[]>();
-    private List<double> mouse_click_record = new List<double>();
-
-    private double record_start_time = 0;
-    UDPSocketUnity server = new UDPSocketUnity();
+        UDPSocketUnity server = new UDPSocketUnity();
     // Start is called before the first frame update
     void Start()
     {
@@ -58,9 +43,7 @@ public class BalanceBoardSensor : MonoBehaviour
         server.Server("127.0.0.1", 27335, this);
     }
     //record when mouse was clicked
-    void MouseClickRecord(){
-        mouse_click_record.Add(DateTime.Now.TimeOfDay.TotalMilliseconds - record_start_time);
-    }
+    
     void FixedUpdate()
     {
       //if record_headset flag is true, record vertical height of headset
@@ -72,221 +55,13 @@ public class BalanceBoardSensor : MonoBehaviour
             weight = (weight + weight_reading)/num_weight_readings;
         }
     }
-    //public wrapper for Unity Debug
-    public void display(string s)
-    {
-        Debug.Log(s);
-    }
+    
     //public wrapper to add method of latency list
     public void AddLatency(double latency)
     {
         latencies.Add(latency);
     }
 
-    //toggle recording headset height and board sensors
-    public void ToggleRecordHeadsetBoard()
-    {
-        record_board_collision = !record_board_collision;
-        record_headset = !record_headset;
-        record_start_time = DateTime.Now.TimeOfDay.TotalMilliseconds;
-    }
-    //toggle recording board sensors
-    public void ToggleRecordBoard(){
-        record_board_collision = !record_board_collision;
-        record_start_time = DateTime.Now.TimeOfDay.TotalMilliseconds;
-    }
-    //save latency data to data.csv
-    public void StoreLatencyData()
-    {
-        Debug.Log("Created data file");
-        List<double> fixed_latencies = new List<double>(latencies);
-        string x = "Reading #";
-        string y = "Latency (ms)";
-
-        for (int i = 0; i < fixed_latencies.Count; i++)
-        {
-            x += "," + i;
-            y += "," + fixed_latencies[i];
-        }
-
-        using (StreamWriter writer = new StreamWriter("Assets/BalanceBoard/data.csv"))
-        {
-            writer.WriteLine(x);
-            writer.WriteLine(y);
-            writer.WriteLine(avglatency);
-        }
-    }
-    //save mouse + board sensor data
-    public void SaveMouseBoardData(){
-        List<double[]>[] raw_data = {
-                                        new List<double[]>(board_recording[0]),
-                                        new List<double[]>(board_recording[1]),
-                                        new List<double[]>(board_recording[2]),
-                                        new List<double[]>(board_recording[3])
-                                        };
-        List<double> mouse_data = new List<double>(mouse_click_record);
-        List<string> lines = new List<string>();
-        for (int i = 0; i < raw_data.Length; i++)
-        {
-            string output_line = "";
-            string time = "Time";
-            switch (i)
-            {
-                case 0:
-                    output_line = "Top Left raw data";
-                    break;
-                case 1:
-                    output_line = "Top Right raw data";
-                    break;
-                case 2:
-                    output_line = "Bottom Left raw data";
-                    break;
-                case 3:
-                    output_line = "Bottom Right raw data";
-                    break;
 
 
-
-            }
-            for (int j = 0; j < raw_data[i].Count; j++)
-            {
-                output_line += ", " + raw_data[i][j][0];
-                time += ", " + raw_data[i][j][1];
-            }
-            lines.Add(output_line);
-            lines.Add(time);
-        }
-        string mouse_line = "Mouse Click Times";
-        foreach (double timestamp in mouse_data)
-        {
-            mouse_line+= "," + timestamp;
-
-        }
-        lines.Add(mouse_line);
-        using (StreamWriter writer = new StreamWriter("Assets/BalanceBoard/mouse.csv"))
-        {
-            foreach (string item in lines)
-            {
-                writer.WriteLine(item);
-            }
-        }
-Debug.Log("Done");
-    }
-
-    //save headset and board data
-    public void StoreVRvsBoardReadings()
-    {
-        Debug.Log("Created data file");
-
-        List<double[]>[] raw_data = {
-                                        new List<double[]>(board_recording[0]),
-                                        new List<double[]>(board_recording[1]),
-                                        new List<double[]>(board_recording[2]),
-                                        new List<double[]>(board_recording[3])
-                                        };
-        List<double[]> headset = new List<double[]>(Headset_record);
-        List<string> lines = new List<string>();
-        for (int i = 0; i < raw_data.Length; i++)
-        {
-            string output_line = "";
-            string time = "Time";
-            switch (i)
-            {
-                case 0:
-                    output_line = "Top Left raw data";
-                    break;
-                case 1:
-                    output_line = "Top Right raw data";
-                    break;
-                case 2:
-                    output_line = "Bottom Left raw data";
-                    break;
-                case 3:
-                    output_line = "Bottom Right raw data";
-                    break;
-
-
-
-            }
-            for (int j = 0; j < raw_data[i].Count; j++)
-            {
-                output_line += ", " + raw_data[i][j][0];
-                time += ", " + raw_data[i][j][1];
-            }
-            lines.Add(output_line);
-            lines.Add(time);
-        }
-        string headset_height = "Headset Height";
-        string headset_time = "Time";
-        for (int i = 0; i < headset.Count; i++)
-        {
-            headset_height += ", " + Headset_record[i][0];
-            headset_time += ", " + Headset_record[i][1];
-        }
-        lines.Add(headset_height);
-        lines.Add(headset_time);
-        using (StreamWriter writer = new StreamWriter("Assets/BalanceBoard/headset.csv"))
-        {
-            foreach (string item in lines)
-            {
-                writer.WriteLine(item);
-            }
-        }
-Debug.Log("Done");
-
-    }
-
-
-    //Record headset vertical height
-    public void RecordHeadsetVertical()
-    {
-        try
-        {
-          //take difference from starttime and current time
-             Headset_record.Add(new double[] { (double) headsetcamera.position.y, DateTime.Now.TimeOfDay.TotalMilliseconds-record_start_time });
-        }
-        catch (System.Exception e)
-        {
-            Debug.Log(e);
-            throw;
-        }
-
-
-    }
-    public void toggleWeightCalculation(){
-        calculateWeight = !calculateWeight;
-    }
-    //record raw data from balance board
-    public void Record_Raw(string msg_parts)
-    {
-      //get corresponding index of sensor to list of sensor readings, and get current value of that sensor
-        int i;
-        double value;
-        switch (msg_parts)
-        {
-
-            case "rTL":
-                value = rwTopLeft;
-                i = 0;
-                break;
-            case "rTR":
-                value = rwTopRight;
-                i = 1;
-                break;
-            case "rBL":
-                value = rwBottomLeft;
-                i = 2;
-                break;
-            case "rBR":
-                value = rwBottomRight;
-                i = 3;
-                break;
-            default:
-                return;
-
-        }
-        //add reading to list, along with timestamp of data collection
-        board_recording[i].Add(new double[] { (double)value, DateTime.Now.TimeOfDay.TotalMilliseconds-record_start_time });
-
-    }
 }
--- BalanceBoard/UDPSocketUnity.cs
+++ BalanceBoard/UDPSocketUnity.cs
@@ -31,14 +31,14 @@ public class UDPSocketUnity : UDPSocket.UDPSocket
             }
             else
             {
-              //Recieving balanceboard client data
+                //Recieving balanceboard client data
                 string msg = Encoding.ASCII.GetString(so.buffer, 0, bytes);
                 string[] msg_parts = msg.Split(':');
                 // all data will be sent as sensor_name:sensor_value, or in case of latency testing,
                 // timestamp:timestamp-value
                 switch (msg_parts[0])
                 {
-                  //for all sensor data, update balanceboard script attributes
+                    //for all sensor data, update balanceboard script attributes
                     case "rTL":
                         unityScript.rwTopLeft = double.Parse(msg_parts[1]);
                         break;
@@ -53,50 +53,10 @@ public class UDPSocketUnity : UDPSocket.UDPSocket
                         break;
 
                     case "timestamp":
-                      //get current time in milliseconds
-                        double curr_ms = DateTime.Now.TimeOfDay.TotalMilliseconds;
-                        //get time data was sent out in milliseconds
-                        double received_ms = Double.Parse(msg_parts[1]);
-                        //compute latency
-                        double latency = curr_ms - received_ms;
-                        //assign latency variables of balance board
-                        unityScript.latency = latency;
-                        unityScript.AddLatency(latency);
-                        //assign min and max latency when applicable
-                        if (latency < unityScript.minlatency) unityScript.minlatency = latency;
-                        if (latency > unityScript.maxlatency) unityScript.maxlatency = latency;
-                        //catch impossible latencies - this could should never run
-                        if (latency < 0){
-                          unityScript.display("Impossible Latency");
-                            unityScript.display(latency + ", curr ms : " + curr_ms + ", received ms : " + received_ms + ", exact string : " + msg_parts[1]);
-                        }
-                        //if we have less than 2 previous latencies, cannot calculate mean
-                        if (unityScript.latency_count < 2)
-                        {
-                          //just set average latency to current- not enough samples
-                            unityScript.avglatency = latency;
-                            if (unityScript.latency_count == 1)
-                            {
-                              //set average delay between readings to current delay- not enough samples to produce average
-                                unityScript.avg_ms_per_reading = curr_ms - unityScript.prev_ms;
-
-                            }
-
-                        }
-                        else
-                        {
-                          //have enough samples to start computing a dynamic mean for both sensor latencies and sensor capture rates
-                            unityScript.avglatency = ((unityScript.avglatency * (unityScript.latency_count - 1)) + latency) / unityScript.latency_count;
-                            unityScript.avg_ms_per_reading = ((unityScript.avg_ms_per_reading * (unityScript.latency_count - 1)) + (curr_ms - unityScript.prev_ms)) / unityScript.latency_count;
-                        }
-                        //update incremental values
-                        unityScript.prev_ms = curr_ms;
-                        unityScript.latency_count += 1;
+                        monitor_latency(msg_parts);
                         break;
                 }
-                //if recording board collision, send collided sensor to RecordBoard
-                if (unityScript.record_board_collision)
-                    unityScript.Record_Raw(msg_parts[0]);
+
 
             }
 
@@ -104,6 +64,48 @@ public class UDPSocketUnity : UDPSocket.UDPSocket
 
         }, state);
     }
+    void monitor_latency(string[] msg_parts)
+    {
+        //get current time in milliseconds
+        double curr_ms = DateTime.Now.TimeOfDay.TotalMilliseconds;
+        //get time data was sent out in milliseconds
+        double received_ms = Double.Parse(msg_parts[1]);
+        //compute latency
+        double latency = curr_ms - received_ms;
+        //assign latency variables of balance board
+        unityScript.latency = latency;
+        unityScript.AddLatency(latency);
+        //assign min and max latency when applicable
+        if (latency < unityScript.minlatency) unityScript.minlatency = latency;
+        if (latency > unityScript.maxlatency) unityScript.maxlatency = latency;
+        //catch impossible latencies - this could should never run
+        if (latency < 0)
+        {
+            unityScript.display("Impossible Latency");
+            unityScript.display(latency + ", curr ms : " + curr_ms + ", received ms : " + received_ms + ", exact string : " + msg_parts[1]);
+        }
+        //if we have less than 2 previous latencies, cannot calculate mean
+        if (unityScript.latency_count < 2)
+        {
+            //just set average latency to current- not enough samples
+            unityScript.avglatency = latency;
+            if (unityScript.latency_count == 1)
+            {
+                //set average delay between readings to current delay- not enough samples to produce average
+                unityScript.avg_ms_per_reading = curr_ms - unityScript.prev_ms;
 
+            }
+
+        }
+        else
+        {
+            //have enough samples to start computing a dynamic mean for both sensor latencies and sensor capture rates
+            unityScript.avglatency = ((unityScript.avglatency * (unityScript.latency_count - 1)) + latency) / unityScript.latency_count;
+            unityScript.avg_ms_per_reading = ((unityScript.avg_ms_per_reading * (unityScript.latency_count - 1)) + (curr_ms - unityScript.prev_ms)) / unityScript.latency_count;
+        }
+        //update incremental values
+        unityScript.prev_ms = curr_ms;
+        unityScript.latency_count += 1;
+    }
 
 }
