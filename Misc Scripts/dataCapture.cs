cb7e974f4931ccf2471832de8270a4a6ceac9f6a
Ludo Attuoni <ludoattuoni@gmail.com>
2022-03-14 13:21:03 +0000

    Implemented trials

--- Misc Scripts/dataCapture.cs
+++ Misc Scripts/dataCapture.cs
@@ -10,36 +10,75 @@ public class dataCapture : MonoBehaviour
 {
     [SerializeField] Transform Head;
     [SerializeField] BalanceBoardSensor bbs;
-    [SerializeField] COMSway COMSway;
+
+    [SerializeField] PendulumMovement pendulum;
+    [SerializeField] float control_duration, test_duration, test_sensitivity, aftermath_duration;
     bool recording = false;
-    double starttime;
-    List<double[]> board_sensors = new List<double[]>();
+    float t;
+    List<double[]>  board_sensors   = new List<double[]>();
+    List<float>     sinwave_vals    = new List<float>();
+    List<Vector3>   head_positions  = new List<Vector3>();
+    public Trial_Stages stage;
 
-    List<float> sinwave_vals = new List<float>(), time_stamp = new List<float>();
-    List<Vector3> head_positions = new List<Vector3>();
-
-    // Start is called before the first frame update
-    void ToggleRecordData(){
-        if(recording){
-            recording = false;
-            
-            // write all stored data
-        }else{ starttime = DateTime.Now.TimeOfDay.TotalMilliseconds;recording = true;}
-    }
-
-    // Update is called once per frame
-    void Update()
+    public enum Trial_Stages { Control_Stage, Test_Stage, Aftermath_Stage }
+    void BeginTrial()
     {
-        if(recording){
+        recording = true;
+        stage = Trial_Stages.Control_Stage;
+        pendulum.sensitivity = 0;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (recording)
+        {
+            switch (stage)
+            {
+                case Trial_Stages.Control_Stage:
+                    if (t < control_duration)
+                    {
+                        t += Time.deltaTime;
+                    }
+                    else
+                    {
+                        stage = Trial_Stages.Test_Stage;
+                        pendulum.sensitivity = test_sensitivity;
+                    }
+                    break;
+
+                case Trial_Stages.Test_Stage:
+                    if (t < control_duration + test_duration)
+                    {
+                        t += Time.deltaTime;
+                    }
+                    else
+                    {
+                        stage = Trial_Stages.Aftermath_Stage;
+                        pendulum.sensitivity = 0;
+                    }
+                    break;
+
+                case Trial_Stages.Aftermath_Stage:
+                    if (t < control_duration + test_duration + aftermath_duration)
+                    {
+                        t += Time.deltaTime;
+                    }
+                    else
+                    {
+                        Debug.Log("Fin");
+                        recording = false;
+                    }
+                    break;
+            }
+
             // record bbs
-            board_sensors.Add(new double[]{bbs.rwTopLeft, bbs.rwBottomRight, bbs.rwBottomLeft, bbs.rwBottomRight});
-            //record sinwave
-            sinwave_vals.Add(COMSway.sinwave_position);
+            board_sensors.Add(new double[] { bbs.rwTopLeft, bbs.rwBottomRight, bbs.rwBottomLeft, bbs.rwBottomRight });
             //record head
             head_positions.Add(Head.position);
-
-            //record timestamp
-            //time_stamp.Add(DateTime.Now.TimeOfDay.TotalMilliseconds- starttime);
         }
+    }
+    void write_data(){
+        using (StreamWriter writer = new StreamWriter("Assets/BalanceBoard/mouse.csv"))
+        {}
     }
 }
